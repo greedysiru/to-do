ecfea3a5fa92c28a320b5a48d911cf58a218e850
Jaemin Jeon <greedysiru@gmail.com>
2022-07-21 02:17:42 +0900

    FEAT : 5.18 Enums

--- src/components/atmos.tsx
+++ src/components/atmos.tsx
@@ -1,11 +1,25 @@
 import { atom, selector } from "recoil";
 
+// enumerable
+// 일련의 숫자를 문자로 표현해 줌
+export enum Categories {
+  "TO_DO" = "TO_DO", // 0
+  "DOING" = "DOING", // 1
+  "DONE" = "DONE", // 2
+}
+
 export interface IToDo {
   text: string;
   id: number;
   // 세 가지의 문자열만 받을 수 있음
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
+
+export const categoryState = atom<IToDo["category"]>({
+  key: "category",
+  // 정의한 enum에 접근
+  default: Categories.TO_DO,
+});
 
 // 제네릭으로 인터페이스를 전달
 export const toDoState = atom<IToDo[]>({
@@ -21,11 +35,8 @@ export const toDoSelector = selector({
   get: ({ get }) => {
     // atom을 가져옴
     const toDos = get(toDoState);
+    const selectedCategory = get(categoryState);
     // 여기서 return하는 값이 이 selector의 value가 됨
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    return [...toDos.filter((toDo) => toDo.category === selectedCategory)];
   },
 });
